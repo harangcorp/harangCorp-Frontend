ab3d5be76fedcaa8d77dcadee96babf3aa037bea
오명훈 <omh02033@gmail.com>
2022-09-11 19:57:07 +0900

    feat: react test page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import harangLogo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div>
+      <img src={harangLogo} alt="" />
+      <h1>Harang Corp</h1>
+    </div>
+  );
+}
+
+export default App;
