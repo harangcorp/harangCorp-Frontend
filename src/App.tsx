34a5a41c6f637daebd86fe4c5282bafeb00b296d
오명훈 <omh02033@gmail.com>
2022-09-13 15:18:04 +0900

    feat: pdf view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import harangLogo from './assets/logo.svg';
+import { Document, Page, pdfjs } from 'react-pdf';
+import { useState } from 'react';
+import useWindowSize from "./windowSize";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
+  const windowSize = useWindowSize();
+  const [numPages, setNumPages] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
   return (
     <div>
-      <img src={harangLogo} alt="" />
-      <h1>Harang Corp</h1>
+      <Document file="/src/assets/harang.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page
+            width={windowSize.width}
+            height={windowSize.height}
+            key={index}
+            pageNumber={index + 1}
+            renderAnnotationLayer={false}
+          />
+        ))}
+      </Document>
     </div>
   );
 }
